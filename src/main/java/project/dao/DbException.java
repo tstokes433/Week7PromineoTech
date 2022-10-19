ff4722db108742a247a26ad3280e4abac6354321
Travis Stokes <tstokes433@gmail.com>
2022-10-19 15:22:12 -0500

    first commit

---
+++ src/main/java/project/dao/DbException.java
@@ -0,0 +1,21 @@
+package project.dao;
+
+@SuppressWarnings("serial")
+public class DbException extends RuntimeException {
+
+	public DbException() {
+	}
+
+	public DbException(String message) {
+		super(message);
+	}
+
+	public DbException(Throwable cause) {
+		super(cause);
+	}
+
+	public DbException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
---
+++ src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,14 @@
+package projects;
+
+//import java.sql.Connection;
+
+import project.dao.DbConnection;
+
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+	DbConnection.getConnection();	
+
+	}
+
+}
